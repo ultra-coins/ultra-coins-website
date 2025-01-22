6bf7bea85a6a1ac701d7cebe0336a7d0812638a8
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-01-22 23:44:02 +0900

    feat: change flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,26 @@
 {
-  description = "zola environment";
+  description = "cloudflare-r2-sdk";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
       in
       {
         devShells.default =
@@ -18,7 +28,9 @@
           mkShell {
             buildInputs = [
               zola
+              rust-bin.beta.latest.default
             ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
           };
       }
     );
